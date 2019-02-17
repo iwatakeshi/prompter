dfc5f0dac01cccf77b32a6e3397bccee4ba24364
Takeshi <iwatakeshi@gmail.com>
2019-02-17 16:57:42 -0700

    Update main.dart
    Remove commented line

--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,6 @@ import 'package:prompter/prompter.dart';
 import 'package:prompter/src/option.dart';
 
 main(List<String> arguments) {
-  // print('Hello world: ${prompter.calculate()}!');
   final prompter = Prompter();
   var value = prompter.choice("What's you're favorite color?", [
     Option("It's red!", "#f00"),
